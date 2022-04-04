8f34d7cd215d55ebfd4e4ab4469e6f31ab05a3f6
Adriano Dantas <adrianodts@msn.com>
2022-04-03 23:05:04 -0300

    created domain event for product

---
+++ src/domain/event/product/handler/send-email-when-product-created-handler.ts
@@ -0,0 +1,8 @@
+import EventHandlerInterface from '../../@shared/event-handler-interface';
+import ProductCreated from '../product-created';
+
+export default class SendEmailWhenProductCreatedHandler implements EventHandlerInterface<ProductCreated> {
+  handle(event: ProductCreated): void {
+    console.log(`Send email for product created event : ${event.eventData.name}`);
+  }
+} 
